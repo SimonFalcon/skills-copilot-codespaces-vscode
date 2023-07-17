95c9782a941ca99429a46b2a5d43cd3aaae5496f
SimonFalcon <112114207+SimonFalcon@users.noreply.github.com>
2023-07-17 19:05:32 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+    var member = {
+        name: 'John Doe',
+        age: 30,
+        skills: ['HTML', 'CSS', 'JS'],
+        calculateNumbers: function(var1, var2) {
+            return var1 + var2;
+        }
+    }
+    return member;
+}
